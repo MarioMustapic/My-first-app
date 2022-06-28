8dd9986a96157814fa2d5904974e3d3404c4ecca
Mario Mustapic <mariomustapicvkci@gmail.com>
2022-06-28 21:03:06 +0200

    More buttons!

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import "./App.css"
+import "./App.css";
 import { InputElement} from "./components/InputElement";
 import Button from "./components/Button";
 import FormMessage from "./components/FormMessage";
 import FormError from "./components/FormError";
-import {User} from "./components/User"
-
+import {User} from "./components/User";
+import {Counter} from "./components/Counter";
 
 function App() {
   const isSignedIn = true;
   
   return (
     <div>
+      <Counter startingState={0}/>
       {isSignedIn && <User username={"Mario Mustapić"} src={"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/208A0E5EB8E1ADCEE6DEE4149CFC1428BDAABFCE5A006D2F240ADD8B87F239A5/scale?width=1200&aspectRatio=1.78&format=jpeg"}/>
       }
     <form className="form">
---
+++ src/components/Counter.jsx
@@ -0,0 +1,24 @@
+import "./Counter.css";
+import { useState } from "react";
+
+export function Counter (props) {
+    const counterState = useState(props.startingState);
+    const counter = counterState[0];
+    const setCounter = counterState[1];
+
+    const handleClickPlus = () => {
+        setCounter((state) => ++state);
+    }
+    const handleClickMinus = () => {
+        setCounter((state) => --state);
+    }
+
+
+    return (
+    <div>
+        <button className="counter" onClick={handleClickMinus}>-</button>
+        <button className="counter" >{counter}</button>
+        <button className="counter" onClick={handleClickPlus}>+</button>
+    </div>
+    );
+}
