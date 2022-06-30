c4098e5da36549fc2ed4932d712ae71c16017101
Mario Mustapic <mariomustapicvkci@gmail.com>
2022-06-30 19:37:51 +0200

    set message state

--- src/pages/ChatRoomPage.jsx
+++ src/pages/ChatRoomPage.jsx
@@ -1,11 +1,41 @@
 import Button from "../components/Button";
+import { InputElement } from "../components/InputElement";
 import { User } from "../components/User";
+import { useState } from "react";
 
 export function ChatRoomPage(props) {
+  const [formState, setFormState] = useState({ message: '' });
+  const [messages, setMessages] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (formState.message !== '') {
+      setMessages((state) => [...state, formState]);
+    }
+  }
+  
+  const handleChange = (event) => {
+    setFormState((state) => ({
+          ...state,
+          [event.target.name]: event.target.value,
+    }));
+  }
+
+  console.log(messages);
+
   return (
     <div>
       <Button type="button" onClick={props.onSignOut}>Sign out</Button>
       <User src="/avatar.svg" username={props.user.username} />
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-field">
+          <InputElement name="message" label="Message" type="text" onChange={handleChange} />
+        </div>
+        <div className="form-field">
+          <Button type="submit">Send</Button>
+        </div>
+      </form>
     </div>
   );
 }
