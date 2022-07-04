e2c1672e2a7cd75a2814a8632b5ab3d656b6d6d5
Mario Mustapic <mariomustapicvkci@gmail.com>
2022-07-04 18:28:58 +0200

    Added change username

--- src/pages/ChatRoomPage.jsx
+++ src/pages/ChatRoomPage.jsx
@@ -1,22 +1,23 @@
 import Button from "../components/Button";
 import { InputElement } from "../components/InputElement";
 import { User } from "../components/User";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export function ChatRoomPage(props) {
   const [formState, setFormState] = useState({ message: '' });
   const [messages, setMessages] = useState([]);
-  const authorOfMessage = useState()
-
-  useEffect(() => {
-    console.log('New message stored!');
-  }, [messages]);
+  const [settingsFormState, setSettingsFormState] = useState({
+    username: props.user.username,
+  });
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
     if (formState.message !== '') {
-      setMessages((state) => [...state, {...formState, author:props.user.username}]);
+      setMessages((state) => [
+        ...state,
+        { ...formState, author: props.user.username },
+      ]);
       setFormState({ message: '' });
     }
   }
@@ -28,16 +29,44 @@ export function ChatRoomPage(props) {
     }));
   }
 
+  const handleSettingsSubmit = (event) => {
+    event.preventDefault();
+    console.log(settingsFormState);
+  }
+
+  const handleSettingsChange = (event) => {
+    setSettingsFormState((state) => ({
+      ...state,
+      [event.target.name]: event.target.value,
+    }));
+  }
+
   const messageElements = messages.map((item, index) => (
     <div key={index}>
       <div>{item.author}</div>
       <div>{item.message}</div>
     </div>
   ));
+
   return (
     <div>
       <Button type="button" onClick={props.onSignOut}>Sign out</Button>
-      <User src="/avatar.jpg" username={props.user.username} />
+      <User src="/avatar.svg" username={props.user.username} />
+
+      <form className="form" onSubmit={handleSettingsSubmit}>
+        <div className="form-field">
+          <InputElement name="username"
+            label="Username"
+            type="text"
+            onChange={handleSettingsChange}
+            value={settingsFormState.username}
+          />
+        </div>
+        <div className="form-field">
+          <Button type="submit">Update</Button>
+        </div>
+      </form>
+
       <form className="form" onSubmit={handleSubmit}>
         <div className="form-field">
           <InputElement name="message"
