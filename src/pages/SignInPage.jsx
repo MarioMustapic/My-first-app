2e8338ff8eff72c757b4c90217c29294ffdb280f
Mario Mustapic <mariomustapicvkci@gmail.com>
2022-07-08 18:54:22 +0200

    Add NASA photo of the day component

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -4,6 +4,7 @@ import { SignInForm } from "../components/SignInForm";
 import { useContext } from "react";
 import { AppContext } from "../contexts/AppContext";
 import { Link } from "react-router-dom";
+import { NasaPhotoOfTheDay } from "../components/NasaPhotoOfTheDay";
 
 export function SignInPage() {
   const {storeUser} = useContext(AppContext);
@@ -14,9 +15,9 @@ export function SignInPage() {
 
   return (
     <div className="sign-in-page">
-        <SignInForm onSubmit={handleSubmit} />
-        <Link to="/tos">Terms of Service</Link>
-        <Link to="placeholder">placeholder</Link>
+      <NasaPhotoOfTheDay />
+      <SignInForm onSubmit={handleSubmit} />
+      <Link to="/tos">Terms of Service</Link>
     </div>
   );
 }
